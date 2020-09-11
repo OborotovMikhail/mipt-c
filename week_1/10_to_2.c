51aed1c270d529df2eafed1aae7da1302cfe54c4
OborotovMikhail <thejarveyz@gmail.com>
2020-09-11 04:26:01 +0300

    week 1 completed

--- week_1/10_to_2.c
+++ week_1/10_to_2.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
+#include <math.h>
+
+int func_10to2 (int input) {
+	int res = 0, left, n = 0;
+
+	while (input > 0) {
+		left = input % 2;
+
+		res = res + left * pow(10, n);
+		n++;
+
+		if (left == 0) {
+			res = res * 10;
+		}
+
+		//printf("test - %d\n", res);
+
+		input = input / 2;
+	}
+
+	return res;
+}
 
 int main()
 {
-	
+	int x;
+	scanf("%d", &x);
+	printf("\n%d\n", func_10to2(x));
 	return 0;
 }
--- week_1/2_to_10.c
+++ week_1/2_to_10.c
@@ -1,7 +1,59 @@
 #include <stdio.h>
+#include <math.h>
+
+int func_2to10 (int input) {
+	int res = 0, staff, len = 0, plus, current;
+	staff = input;
+
+	while (staff > 0) {
+		staff = staff / 10;
+		len++;
+	}
+
+	input = input + pow(10, len);
+	//printf("\nnew input = %d\n", input);
+
+	int full_len = len;
+	int big = pow(10, len);
+	int process = input - big;
+	//printf("process = %d\n", process);
+
+	len = len - 1;
+	//printf("len = %d\n", len);
+
+	while (process > 0) {
+		//printf("\ninput: %d\n", input);
+
+		plus = input / pow(10, len);
+		current = pow(10, full_len - len);
+		//printf("plus - current: %d - %d = ", plus, current);
+		plus = plus - current;
+		//printf("%d\n", plus);
+		plus = plus * pow(2, len);
+
+		//printf("BODY: %d + %d = ", res, plus);
+
+		res = res + plus;
+
+		//printf("%d\n", res);
+
+		if (plus != 0) {
+			input = input - pow(10, len);
+		}
+		len--;
+		//printf("input at the end: %d\n", input);
+
+		process = input - big;
+		//printf("process: %d\n", process);
+	}
+
+	return res;
+}
 
 int main()
 {
-	
+	int x;
+	scanf("%d", &x);
+	printf("\n%d\n", func_2to10(x));
 	return 0;
 }
